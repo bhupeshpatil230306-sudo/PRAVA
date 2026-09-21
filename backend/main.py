4876c6dfba342adf0b4025632d6d3f62a19402c8
bhupeshpatil-sudo <bhupeshpatil230306@gmail.com>
2026-09-21 19:15:08 +0530

    Fix production CORS

--- backend/main.py
+++ backend/main.py
@@ -17,7 +17,10 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
+    allow_origins=[
+    "http://localhost:3000",
+    "https://prava-1-30hu.onrender.com"
+],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
